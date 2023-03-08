823abbd892212b137b03aea363123ce512692e61
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-08 09:21:54 +0100

    Ajoutez des fichiers projet.

---
+++ Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAnimal
+{
+    public class Animal
+    {
+        private string Name { get; set; }
+
+        public Animal(string name) 
+        {
+            Name = name;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"{Name} move");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} sleep");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleAppAnimal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+
+        //Création Animal
+        Animal Chien = new Animal(name);
+
+        //affiche {Name} + move
+        Chien.Move();
+
+        ////affiche {Name} + sleep
+        Chien.Sleep();
+    }
+}
