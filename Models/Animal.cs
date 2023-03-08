98cbdbf2a78812a03dd09faf70e4c7c761ccba78
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-08 11:27:13 +0100

    Griffin Etape 3

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -4,31 +4,49 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleAppAnimal.Models
 {
-    public class Animal
+    public abstract class Animal
     {
-        private string Name { get; set; }
+        protected string Name { get; set; }
 
+        //nom par défaut
         public Animal()
         {
             this.Name = "Clifford";
         }
 
-        public void Move()
-        {
-            Console.WriteLine($"{Name} move");
-        }
+        //move base
+        public abstract void Move();
 
+        //sleep base
         public void Sleep()
         {
             Console.WriteLine($"{Name} sleep");
         }
 
+        //nom définit
         public Animal(string name)
         {
             this.Name = name;
+        }
+    }
+
+    public class Griffin : Animal
+    {
+        public Griffin() : base()
+        {
+        }
+
+        public Griffin(string name) : base(name)
+        {
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} vole");
         }
     }
 }
