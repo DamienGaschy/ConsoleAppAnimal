4f6d32b8b7c4c5f37ae5238b72674448a2729821
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-08 15:16:48 +0100

    t

---
+++ Models/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAnimal.Models
+{
+    class Vehicule
+    {
+        public Vehicule(string name)
+        {
+            this.name = name;
+        }
+
+        public string name { get; set; }
+
+        public virtual void embarquer()
+        {
+            Console.WriteLine("On embarque !");
+        }
+        public virtual void avancer()
+        {
+            Console.WriteLine("On avance !");
+        }
+        public virtual void reculer()
+        {
+            Console.WriteLine("On recule !");
+        }
+    }
+}
